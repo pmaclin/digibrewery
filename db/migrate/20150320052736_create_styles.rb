0708d47139f599081e7555f2d0aa8497c836839c
pmaclin <pcharlesantonio@gmail.com>
2015-03-20 00:27:50 -0500

    Scaffolds Style model

---
+++ db/migrate/20150320052736_create_styles.rb
@@ -0,0 +1,11 @@
+class CreateStyles < ActiveRecord::Migration
+  def change
+    create_table :styles do |t|
+      t.string :name
+      t.text :description
+      t.string :image
+
+      t.timestamps null: false
+    end
+  end
+end
